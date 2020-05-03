9485ad6c10968bbcecefa2ec992596e439d2c327
Alexander <ezharjan@aliyun.com>
2020-05-03 20:02:59 +0800

    project completed

---
+++ Assets/Resources/Script/MagicCircle.cs
@@ -0,0 +1,22 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//  控制魔法阵渐入渐出动画
+public class MagicCircle : MonoBehaviour
+{
+	public RayTrace rayTrace;
+
+	void OnSelect()
+	{
+		DOTween.To(()=> rayTrace.MagicAlpha, a => rayTrace.MagicAlpha = a, 0.2f, 1);
+		var t = DOTween.To(() => rayTrace.MagicAlpha, a => rayTrace.MagicAlpha = a, 1, 1);
+		t.SetDelay(1);
+	}
+
+	void OnCancleSelect()
+	{
+		DOTween.To(() => rayTrace.MagicAlpha, a => rayTrace.MagicAlpha = a, 0, 2);
+	}
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Ray Tracing
+![preview](RayTracing.gif)
+
+## This is an example of real-time ray tracing.
+* Use Unity post-processing to achieve ray tracing.
+* Use custom data structures to store models and materials.
+
+教程参考——[知乎链接](https://zhuanlan.zhihu.com/p/45335463)。
+
